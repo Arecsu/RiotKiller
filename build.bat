c179c44e0a1d6f90c30a74dad37e5ddb5b89537b
Alejandro Romano <alejandro9r@gmail.com>
2021-11-04 17:25:02 -0300

    add build.bat

---
+++ build.bat
@@ -0,0 +1,3 @@
+"PATH\TO\mingw32\g++.exe" RiotKiller.cpp -o RiotKiller-invisible.exe -static-libgcc -static-libstdc++ -static -m32 -mwindows
+
+"PATH\TO\mingw32\g++.exe" RiotKiller.cpp -o RiotKiller.exe -static-libgcc -static-libstdc++ -static -m32
